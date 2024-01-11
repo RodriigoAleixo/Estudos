59e93906ba5a6d188250e4a8f07e2a808ddb88c2
Rodrigo de Lima Aleixo <92765981+RodriigoAleixo@users.noreply.github.com>
2024-01-11 14:06:28 -0300

    Desafios python

---
+++ CursoemVídeo/Desafio .py
@@ -0,0 +1,6 @@
+primeiro = input ("Digite o primeiro número: ")
+segundo = input ("Digite o segundo número: ")
+a = int (primeiro)
+b = int (segundo)
+resultado = a + b
+print ("A soma entre ", primeiro, "e ", segundo, "é: ", resultado)
